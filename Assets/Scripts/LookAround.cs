3254e2cee43d197dea714071c946527456c6f401
fredHolberton <10196@holbertonstudents.com>
2025-04-29 20:31:30 +0200

    3. The full tour: fix 2

--- Assets/Scripts/LookAround.cs
+++ Assets/Scripts/LookAround.cs
@@ -7,13 +7,23 @@ public class LookAround : MonoBehaviour
     /// </summary>
     public float speed = 3;
 
+    /// <summary>
+    /// image to make black transition when changing room.
+    /// </summary>
+    public GameObject imageBackground;
+
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButton(0))
         {
+            // Camera movement
             transform.RotateAround(transform.position, -Vector3.up, speed * Input.GetAxis("Mouse X"));
             transform.RotateAround(transform.position, transform.right, speed * Input.GetAxis("Mouse Y"));
+
+            // Image movement for beeing always in front of the camera
+            imageBackground.transform.RotateAround(transform.position, -Vector3.up, speed * Input.GetAxis("Mouse X"));
+            imageBackground.transform.RotateAround(transform.position, transform.right, speed * Input.GetAxis("Mouse Y"));
         }
     }
 }
